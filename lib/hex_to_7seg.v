16bcc5ae8edf00270cbbc59d7dd3ceba81045534
Jeff DeWall <sr.jilarious@fastmail.net>
2020-04-06 19:35:01 -0600

    Changed F and G segments since I had them reversed.

--- lib/hex_to_7seg.v
+++ lib/hex_to_7seg.v
@@ -11,10 +11,10 @@ module hex_to_7seg
     always @(*) 
     begin
         case(i_val) 
-            'h0: o_segVals = 'h3f;
+            'h0: o_segVals = 'h5f;
             'h1: o_segVals = 'h06;
-            'h2: o_segVals = 'h5b;
-            'h3: o_segVals = 'h4f;
+            'h2: o_segVals = 'h3b;
+            'h3: o_segVals = 'h2f;
             'h4: o_segVals = 'h66;
             'h5: o_segVals = 'h6d;
             'h6: o_segVals = 'h7d;
@@ -23,8 +23,8 @@ module hex_to_7seg
             'h9: o_segVals = 'h6f;
             'ha: o_segVals = 'h77;
             'hb: o_segVals = 'h7c;
-            'hc: o_segVals = 'h39;
-            'hd: o_segVals = 'h5e;
+            'hc: o_segVals = 'h59;
+            'hd: o_segVals = 'h3e;
             'he: o_segVals = 'h79;
             'hf: o_segVals = 'h71;
             default: o_segVals = 'h00;
