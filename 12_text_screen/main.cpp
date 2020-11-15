114aaa7da030f805221e179e04394ffcfef02c5a
Jeff DeWall <sr.jilarious@fastmail.net>
2020-11-15 00:21:42 -0700

    Changed simulation to step via space press.

--- 12_text_screen/main.cpp
+++ 12_text_screen/main.cpp
@@ -5,6 +5,7 @@
 #include <chrono>
 #include <thread>
 #include <SFML/Graphics.hpp>
+#include <SFML/Window/Keyboard.hpp>
 
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
@@ -44,6 +45,7 @@ int main(int argc, char **argv)
 {
     Verilated::commandArgs(argc, argv);
     auto tb = std::make_unique<TestBench<Vtop>>();
+    bool advanceFrame = true;
 
     #if DUMP_SINGLE_FRAME
         tb->openTrace("trace.vcd");
@@ -79,45 +81,54 @@ int main(int argc, char **argv)
                 renderWin->close();
         }
 
-        // Handle ticking our module the number of ticks per frame.
-        int horzCounter = 0;
-        int vertCounter = 0;
-        int pixIdx = 0;
-        for(int ii = 0; ii < NumSimulationTicksPerFrame; ii++)
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Space)) {
+            advanceFrame = true;
+        }
+        
+        if(advanceFrame) 
         {
-            tb->tick();
+            advanceFrame = false;
 
-            if(horzCounter < HorzPixelCount && vertCounter < VertPixelCount) 
+            // Handle ticking our module the number of ticks per frame.
+            int horzCounter = 0;
+            int vertCounter = 0;
+            int pixIdx = 0;
+            for(int ii = 0; ii < NumSimulationTicksPerFrame; ii++)
             {
-                // Take the output and create a pixel from it.
-                pixelArray[pixIdx] = (tb->m_core->PIN_3 << 7) | (tb->m_core->PIN_2 << 6) | (tb->m_core->PIN_1 << 5);
-                pixelArray[pixIdx+1] = (tb->m_core->PIN_6 << 7) | (tb->m_core->PIN_5 << 6) | (tb->m_core->PIN_4 << 5);
-                pixelArray[pixIdx+2] = (tb->m_core->PIN_8 << 7) | (tb->m_core->PIN_7 << 6);
-                pixelArray[pixIdx+3] = 0xff;
-                pixIdx+=4;
-            }
+                tb->tick();
 
-            horzCounter++;
-            if(horzCounter >= HorzScanLineCycles) {
-                horzCounter = 0;
-                vertCounter++;
-                if(vertCounter >= VertCycles) {
-                    vertCounter = 0;
+                if(horzCounter < HorzPixelCount && vertCounter < VertPixelCount) 
+                {
+                    // Take the output and create a pixel from it.
+                    pixelArray[pixIdx] = (tb->m_core->PIN_3 << 7) | (tb->m_core->PIN_2 << 6) | (tb->m_core->PIN_1 << 5);
+                    pixelArray[pixIdx+1] = (tb->m_core->PIN_6 << 7) | (tb->m_core->PIN_5 << 6) | (tb->m_core->PIN_4 << 5);
+                    pixelArray[pixIdx+2] = (tb->m_core->PIN_8 << 7) | (tb->m_core->PIN_7 << 6);
+                    pixelArray[pixIdx+3] = 0xff;
+                    pixIdx+=4;
+                }
+
+                horzCounter++;
+                if(horzCounter >= HorzScanLineCycles) {
+                    horzCounter = 0;
+                    vertCounter++;
+                    if(vertCounter >= VertCycles) {
+                        vertCounter = 0;
+                    }
                 }
             }
+
+            // Clear screen
+            renderWin->clear(sf::Color(128, 255, 255, 255));
+
+            console->info("frame {}\n", frameCounter);
+            frameCounter++;
+
+            sf::Image img;
+            img.create(HorzPixelCount, VertPixelCount, (sf::Uint8 *)pixelArray);
+            texture.update(img);
+
+            sprite.setTexture(texture);
         }
-
-        // Clear screen
-        renderWin->clear(sf::Color(128, 255, 255, 255));
-
-        console->info("frame {}\n", frameCounter);
-        frameCounter++;
-
-        sf::Image img;
-        img.create(HorzPixelCount, VertPixelCount, (sf::Uint8 *)pixelArray);
-        texture.update(img);
-
-        sprite.setTexture(texture);
 
         renderWin->draw(sprite);
         // Update the window
